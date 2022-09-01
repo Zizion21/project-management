ff18337a654f81e4f406b1de9291b829be3699f0
zizion <zeinabjafari9999@gmail.com>
2022-09-01 18:23:18 +0430

    create server and config application

---
+++ app/server.js
@@ -0,0 +1,67 @@
+
+module.exports= class Application{
+    #express= require("express");
+    #app=this.#express();
+    constructor(PORT, DB_URL){
+        this.configDatabase(DB_URL)
+        this.configApplication()
+        this.createServer(PORT)
+        this.createRoutes()
+        this.errorHandler()
+    }
+    
+    configApplication(){
+        const path= require("path");
+        this.#app.use(this.#express.json());
+        this.#app.use(this.#express.urlencoded({extended: true}));
+        this.#app.use(this.#express.static(path.join(__dirname, "..", "public")));
+        
+    }
+    
+    createServer(PORT){
+        const http= require("http");
+        const server= http.createServer(this.#app);
+        server.listen(PORT, ()=>{
+            console.log(`server is running on http://localhost: ${PORT}`);
+        })
+        
+    }
+    
+    configDatabase(DB_URL){
+        const mongoose= require("mongoose");
+        mongoose.connect(DB_URL, (error)=>{
+            if (error) throw error
+            return console.log("connected to the DB seccessfully.");
+        })
+
+    }
+
+    errorHandler(){
+        this.#app.use((req, res, next)=>{
+            return res.status(404).json({
+                status: 404,
+                success: false,
+                message:"صفحه یا آدرس مورد نظر یافت نشد"
+            })
+        });
+        this.#app.use((error, req, res, next)=>{
+            const status= error?.status || 500;
+            const message= error?.message || "internalServerError";
+            return res.status(status).json({
+                status,
+                success: false,
+                message
+            })
+        })
+
+    }
+
+    createRoutes(){
+        this.#app.use("/", (req, res, next)=> {
+            return res.json({
+                message: "this is a new Express Application"
+            })
+        })
+
+    }
+}
